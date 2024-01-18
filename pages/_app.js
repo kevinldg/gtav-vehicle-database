9c7e2e0f3a5e188df40c28e6c8320b22b30bc051
GuNShOtzZ <kevinloeding@outlook.de>
2024-01-18 12:26:09 +0100

    Add header, footer and SWRConfig to layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
-import '@/styles/globals.css'
+import { SWRConfig } from "swr";
+import Footer from "@/components/Footer/Footer";
+import Header from "@/components/Header/Header";
+import "@/styles/globals.css";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <SWRConfig
+      value={{
+        fetcher,
+        refreshInterval: 1000,
+      }}
+    >
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </SWRConfig>
+  );
 }
