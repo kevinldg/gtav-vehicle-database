5d7c3a53bca56f34af32c4ee4dca19bf45401efd
GuNShOtzZ <kevinloeding@outlook.de>
2024-01-18 12:32:12 +0100

    Rename document title and add logo/favicon

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>GTA V Vehicle Database</title>
+        <link rel="icon" href="/img/gta-v-logo.png" sizes="64x64" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
